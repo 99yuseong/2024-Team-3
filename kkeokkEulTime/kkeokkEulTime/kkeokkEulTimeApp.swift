9ef1148bb62dd649000250d1e6f296b2ca1c1c9a
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-06-14 22:30:09 +0900

    Create Xcode Project

---
+++ kkeokkEulTime/kkeokkEulTime/kkeokkEulTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kkeokkEulTimeApp.swift
+//  kkeokkEulTime
+//
+//  Created by 남유성 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct kkeokkEulTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
