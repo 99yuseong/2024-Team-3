a2d5c7aac9be215c745497eb8fb9e96aeb61d48f
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-06-15 01:43:37 +0900

    Feat: 기본 뷰 파일 생성

---
+++ kkeokkEulTime/kkeokkEulTime/ActionSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ActionSelectionView.swift
+//  kkeokkEulTime
+//
+//  Created by 남유성 on 6/15/24.
+//
+
+import SwiftUI
+
+struct ActionSelectionView: View {
+    var body: some View {
+        Text("ActionSelectionView")
+    }
+}
+
+#Preview {
+    ActionSelectionView()
+}
---
+++ kkeokkEulTime/kkeokkEulTime/ActionTutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  ActionTutorialView.swift
+//  kkeokkEulTime
+//
+//  Created by 남유성 on 6/15/24.
+//
+
+import SwiftUI
+
+struct ActionTutorialView: View {
+    var body: some View {
+        Text("ActionTutorialView")
+    }
+}
+
+#Preview {
+    ActionTutorialView()
+}
